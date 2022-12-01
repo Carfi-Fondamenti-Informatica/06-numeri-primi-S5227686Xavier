5b43c36975dcbe2ee03625490e7ef6b0b2c5e752
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-12-01 13:14:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
 int main(){
+  int num;
+    cout<<"inserire il numero che si vuole verificare:"<<endl;
+    cin>>num;
+    if (numeri_primi(2,num)){
+        cout<<"numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
   return 0;
 }
