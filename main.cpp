720d510858f7136fca63e856c02f0729e7e15baa
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-12-03 03:18:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main (){
     int num;
-    int a;
+    bool a;
     cout<<"inserire il numero che si vuole verificare:"<<endl;
     cin>>num;
     a=numeri_primi(1,num);
     if (a==true){
 
         cout<<"numero primo"<<endl;
-    }else{
+    }else if (a==false){
         cout<<"numero non primo"<<endl;
     }
     return 0;
