3383d2ab3766db952434a2eae1a86661e2dcc623
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-12-03 03:14:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,10 @@
 #include "lib.h"
-
-bool numeri_primi(int a){
-    int div=1,conta=0;
-    
-    while(conta<=1 && div<=2){
-        if (n%div==0){
-            conta++;
-        }
-        div++;
-    }
-    if (conta==1){
+bool numeri_primi(int i, int n){
+    if ((n!=2)&&(n%2==0)&&(n!=i)){
+        return false;
+    }else if((n%i==0)&&(n==i)){
         return true;
     }else{
-        return false;
+        return numeri_primi(i+1,n);
     }
 }
